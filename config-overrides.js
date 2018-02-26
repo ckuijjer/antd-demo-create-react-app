c938293ac787ce0d4a23d006a52fcbfde6010aed
Casper Kuijjer <casper@kuijjer.com>
2018-02-26 14:45:35 +0100

    Use of compose

--- config-overrides.js
+++ config-overrides.js
@@ -1,12 +1,10 @@
-const { injectBabelPlugin } = require('react-app-rewired');
+const { compose, injectBabelPlugin } = require('react-app-rewired');
 const rewireLess = require('react-app-rewire-less');
 const theme = require('./theme');
 
-module.exports = function override(config, env) {
-  config = injectBabelPlugin(['import', { libraryName: 'antd', style: true }], config);  // change importing css to less
-  config = rewireLess.withLoaderOptions({
+module.exports = compose(
+  rewireLess.withLoaderOptions({
     modifyVars: theme
-  })(config, env);
-
-  return config;
-};
+  }),
+  config => injectBabelPlugin(['import', { libraryName: 'antd', style: true }], config)
+);
